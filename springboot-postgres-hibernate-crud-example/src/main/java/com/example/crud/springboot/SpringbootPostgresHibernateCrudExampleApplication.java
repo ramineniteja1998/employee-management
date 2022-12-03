220b4b05c5a49d2bcd6fd4e0d6763dd5257f4e6f
Naga Tejaswini <ramineni.naga-tejaswini@capgemini.com>
2022-12-03 21:23:28 +0530

    First Commit

---
+++ springboot-postgres-hibernate-crud-example/src/main/java/com/example/crud/springboot/SpringbootPostgresHibernateCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresHibernateCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresHibernateCrudExampleApplication.class, args);
+	}
+
+}
